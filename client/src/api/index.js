be824b0320e863d6e5b1fe103a53fea9bd33efc2
mathivan07 <mathivan07@gmail.com>
2026-01-06 18:06:36 +0530

    update live url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://govtlinks.onrender.com' });
 
 export default API;
 
